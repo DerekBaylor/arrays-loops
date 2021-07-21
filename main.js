77db4b50a946777e1e3cf34c51091fa90d19740e
Derek Baylor <dbaylor25@gmail.com>
2021-07-20 21:26:03 -0500

    created objects for team members

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
 console.log("setup");
+
+let team5 = [
+    {
+        id: 1,
+        name: "Grace",
+        favoriteColor: "Green",
+        uniqueness: "She's a twin",
+        email: "rgs6090@gmail.com",
+        userImg: "https://avatars.githubusercontent.com/u/78558344?v=4",
+    },
+    {
+        id: 2,
+        name: "Meseret",
+        favoriteColor: "Tumeric Yellow",
+        uniqueness: "9 siblings",
+        email: "nedafh4@gmail.com",
+        userImg: "https://avatars.githubusercontent.com/u/86808221?v=4",
+    },
+    {
+        id: 3,
+        name: "Derek M.",
+        favoriteColor: "Blue",
+        uniqueness: "Arthritc Knees",
+        email: "laxmalone@gmail.com",
+        userImg: "https://avatars.githubusercontent.com/u/77118878?v=4"
+    },
+    {
+        id: 4,
+        name: "Derek B.",
+        favoriteColor: "Green",
+        uniqueness: "Excellent vision",
+        email: "dbaylor25@gmail.com",
+        userImg: "https://avatars.githubusercontent.com/u/83030603?v=4",
+    },
+];
+
+
+team5.forEach(memeberObj) => {
+    console.log(memeberObj.name);
+}
